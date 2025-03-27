80743b21364bb41508aeb86668a32efae377b87f
Basantdigal <basant.digal12@gmail.com>
2025-03-27 23:06:46 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
-// next.config.js
-const repo = 'chiuricosmetics';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',         // Essential for static sites
-  basePath: `/${repo}`,     // Tells Next.js the site prefix
-  assetPrefix: `/${repo}/`, // Helps with assets if needed, often redundant with basePath for simple cases
-  images: {
-    unoptimized: true,     // Required for static export ('next export')
-  },
+  basePath: "/chiuricosmetics",
+  assetPrefix: "/chiuricosmetics/",
 };
 
 module.exports = nextConfig;
